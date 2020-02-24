46d681a54432f459299fa811851f30672de1ed2c
Mikko Xu <mikkoxu@tvunetworks.com>
2020-02-24 05:26:24 -0800

    Add Algebra and its xUnit
    With one failure intentionally

---
+++ Test1/Algebra.cs
@@ -0,0 +1,17 @@
+﻿namespace Test1
+{
+    public class Algebra
+    {
+        public long Add(int num1, int num2)
+        {
+            //return (long)num1 + (long)num2;
+            return num1 + num2;
+        }
+
+        public long Sub(int num1, int num2)
+        {
+            //return (long)num1 - (long)num2;
+            return num1 - num2;
+        }
+    }
+}
---
+++ Test1Tests/AlgebraTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+
+namespace Test1.Tests
+{
+    public class AlgebraTests
+    {
+        [Fact()]
+        public void AddTest1()
+        {
+            Algebra al = new Algebra();
+            Assert.Equal(2, al.Add(1, 1));
+        }
+
+        [Fact()]
+        public void AddTest2()
+        {
+            Algebra al = new Algebra();
+            Assert.Equal(3, al.Add(1, 2));
+        }
+
+        [Fact()]
+        public void AddTest3()
+        {
+            Algebra al = new Algebra();
+            Assert.Equal(2 << 10, al.Add(2 << 9, 2 << 9));
+        }
+
+        [Fact()]
+        public void AddTestMax()
+        {
+            Algebra al = new Algebra();
+            Assert.Equal(Math.Pow(2, 32) - 2, al.Add(int.MaxValue, int.MaxValue));
+        }
+    }
+}
