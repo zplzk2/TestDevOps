438100b4a5493fbac7c7e6a410437f72d6884a62
Mikko Xu <mikkoxu@tvunetworks.com>
2020-02-24 06:57:50 -0800

    Add cobertura test

--- Test1/Algebra.cs
+++ Test1/Algebra.cs
@@ -13,5 +13,16 @@
             //return (long)num1 - (long)num2;
             return num1 - num2;
         }
+
+        public long Multi(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Divide(int num1, int num2)
+        {
+            // TODO: Handle DivideByZeroException
+            return num1 / num2;
+        }
     }
 }
